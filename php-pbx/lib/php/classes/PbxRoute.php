c58444b610b7b60ea75538ddbab04a7b26ce3983
diggydude <jselkins@yahoo.com>
2017-11-13 08:00:08 -0600

    Refactor route class for 8x8 matrix switch vs. 3-layer Clos switch

--- php-pbx/lib/php/classes/PbxRoute.php
+++ php-pbx/lib/php/classes/PbxRoute.php
@@ -7,22 +7,22 @@
 
     protected
 
+      $cache,
       $stations,
-      $busyRoutes;
+      $routes;
       
     public function __construct($params)
     {
       if (!($params->cache instanceof Cache)) {
         throw new Exception(__METHOD__ . ' > Instance of Cache required.');
       }
-      if (!$cache->exists('stations')) {
+      $this->cache = $params->cache;
+      if (!$this->cache->exists('stations')) {
         if (!file_exists($params->ConfigFile)) {
           throw new Exception(__METHOD__ . ' > Configuration file "' . $params->configFile . '" not found.');
         }
-        $stations   = array();
-        $lines      = file($params->configFile);
-        $crosspoint = 0;
-        $connection = 0;
+        $stations = array();
+        $lines    = file($params->configFile);
         foreach ($lines as $line) {
           list($ordinal, $number) = explode("\t", trim($line));
           if (($ordinal < 0) || ($ordinal > 7)) {
@@ -31,111 +31,52 @@
           if (!is_int($number) || (strlen((string) $number) !== 4)) {
             throw new Exception(__METHOD__ . ' > Number must be a 4-digit integer.');
           }
-          if ($ordinal < 4) {
-            $crosspoint = 0;
-            $connection = $ordinal;
-          }
-          else {
-            $crosspoint = 1;
-            $connection = $ordinal - 4;
-          }  
-          $this->stations[$number] = (object) array(
-                                       'crosspoint' => $crosspoint,
-                                       'connection' => $connection
-                                     );
+          $this->stations[$number] = $ordinal;
         }
-        $cache->set('stations', $stations);
+        $this->cache->set('stations', $stations);
       }
-      $this->stations = $cache->get('stations');
-      if (!$cache->exists('busyRoutes')) {
-        $busyRoutes = (object) array(
-                        'ingress' => array(),
-                        'middle'  => array(),
-                        'egress'  => array()
-                      );
-        $cache->set('busyRoutes', $busyRoutes);
+      $this->stations = $this->cache->get('stations');
+      if (!$this->cache->exists('routes')) {
+        $routes = array();
+        for ($x = 0; $x < 8; $x++) {
+          $routes[$x] = array();
+          for ($y = 0; $y < 8; $y++) {
+            $routes[$x][$y] = ($y == $x) ? -1 : 0;
+          }
+        }
+        $this->cache->set('routes', $routes);
       }
-      $this->busyRoutes = $cache->get('busyRoutes');
+      $this->routes = $this->cache->get('routes');
     } // __construct
 
-    protected function getEndoint($number)
+    public function getRoute($callerNumber, $calleeNumber)
     {
-      return $this->stations[$number];
-    } // getEndpoint
+      if ($callerNumber == $calleeNumber) {
+        throw new Exception(__DIR__ . ' > Route from a station to itself not permitted.');
+      }
+      return (object) array(
+               'ax' => $this->stations[$callerNumber],
+               'ay' => $this->stations[$calleeNumber]
+             );
+    } // getRoute
 
-    protected function findRoute($callerNumber, $calleeNumber)
+    public function markRouteBusy($route)
     {
-      $endpoint     = $this->getEndpoint($callerNumber);
-      $ingressRoute = null;
-      for ($i = 0; $i < 4; $i++) {
-        $route = array($endpoint->crosspoint, $endpoint->connection, $i);
-        $hash  = implode("_", $route);
-        if (!in_array($hash, $this->busyRoutes->ingress)) {
-          $this->busyRoutes->ingress[] = $hash;
-          $ingressRoute                = $route;
-          break;
-        }
-      }
-      if ($ingressRoute == null) {
-        return false;
-      }
-      $crosspoint  = $ingressRoute[0];
-      $output      = $ingressRoute[2];
-      $connection  = $crosspoint;
-      $crosspoint  = $output;
-      $middleRoute = null;
-      for ($i = 0; $i < 2; $i++) {
-        $route = array($crosspoint, $connection, $i);
-        $hash  = implode("_", $route);
-        if (!in_array($hash, $this->busyRoutes->middle)) {
-          $this->busyRoutes->middle[] = $hash;
-          $middleRoute                = $route;
-          break;
-        }
-      }
-      if ($middleRoute == null) {
-        array_pop($this->busyRoutes->ingress);
-        return false;
-      }
-      $endpoint    = $this->getEndpoint($calleeNumber);
-      $output      = $endpoint->connection;
-      $crosspoint  = $endpoint->crossPoint;
-      $input       = $middleRoute[0];
-      $egressRoute = null;
-      $route       = array($crosspoint, $input, $output);
-      $hash        = implode("_", $route);
-      if (!in_array($hash, $this->busyRoutes->egress)) {
-        $this->busyRoutes->egress[] = $hash;
-        $egressRoute                = $route;
-      }
-      if ($egressRoute == null) {
-        array_pop($this->busyRoutes->ingress);
-        array_pop($this->busyRoutes->middle);
-        return false;
-      }
-      $route = (object) array(
-                 'ingress' => $ingressRoute,
-                 'middle'  => $middleRoute,
-                 'egress'  => $egressRoute
-               );
-      return $route;
-    } // findRoute
-    
-    protected function mapIngressControlSignal($params)
+      $this->routes[$route->ax][$route->ay] = 1;
+      $this->cache->set('routes', $this->routes);
+    } // markRouteBusy
+
+    public function markRouteNotBusy($route)
     {
-      
-    } // mapIngressControlSignal
-    
-    protected function mapMiddleControlSignal($params)
+      $this->routes[$route->ax][$route->ay] = 0;
+      $this->cache->set('routes', $this->routes);
+    } // markRouteNotBusy
+
+    public function routeIsBusy($route)
     {
-      
-    } // mapMiddleControlSignal
-    
-    protected function mapEgressControlSignal($params)
-    {
-      
-    } // mapEgressControlSignal
-    
+      return ($this->routes[$route->ax][$route->ay] == 1);
+    } // routeIsBusy
+
   } // PbxRoute
 
 ?>
